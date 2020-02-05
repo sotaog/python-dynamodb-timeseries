cd09fdb7f62565cb394d59fcc72685fc8a0cd785
Zachary Fox <zach@sotaog.com>
2020-02-05 09:58:47 -0600

    Added latest function, keep tables in Timeseries to filter queries by

--- dynamodb_timeseries/timeseries.py
+++ dynamodb_timeseries/timeseries.py
@@ -1,15 +1,25 @@
 import logging
 import os
 import sys
+import time
 from decimal import Decimal
 from multiprocessing import Pool
 from typing import List, Union
 
 from dynamodb_timeseries import exceptions, DEFAULT_REGIONS
-from dynamodb_timeseries.dynamodb import create_table, put, put_batch, query
+from dynamodb_timeseries.dynamodb import create_table, list_tables, put, put_batch, query
 from dynamodb_timeseries.tableresolver import MONTHLY, TableResolver
 
 logger = logging.getLogger(__name__)
+
+
+def _latest(table_names, tag):
+    end_ts = int(time.time() * 1000)
+    for table_name in table_names:
+        results = query(table_name, tag, 0, end_ts, limit=1, order='desc')
+        if results:
+            return results
+    return []
 
 
 class TimeSeries:
@@ -24,13 +34,24 @@ class TimeSeries:
 
     def __init__(self, table_name_prefix: str, interval: int = MONTHLY, regions: List[str] = None):
         self.table_name_prefix = table_name_prefix
+        self.__tables = list_tables(table_name_prefix)
         self.tr = TableResolver(table_name_prefix, interval=interval)
         if regions is None:
             regions = self.__get_regions()
         self.regions = regions
 
+    def __create_table(self, table_name: str):
+        create_table(table_name, replicated_regions=self.regions)
+        self.__tables.append(table_name)
+        self.__tables.sort()
+
+    @property
+    def tables(self):
+        return self.__tables
+
     def query(self, tags: List[str], start_timestamp_ms: int, end_timestamp_ms: int, limit: int = 0, order: Union['asc', 'desc'] = 'desc'):
         table_names = self.tr.get_tables(start_timestamp_ms, end_timestamp_ms)
+        table_names = [name for name in table_names if name in self.__tables]
         if order == 'desc':
             table_names.sort(reverse=True)
         results = {}
@@ -54,13 +75,25 @@ class TimeSeries:
                 resp[tag] = resp[tag][0:limit]
         return resp
 
+    def latest(self, tags: List[str]):
+        table_names = self.__tables
+        table_names.sort(reverse=True)
+        results = {}
+        pool = Pool(processes=min(self.MAX_CONCURRENCY, len(tags)))
+        for tag in tags:
+            results[tag] = pool.apply_async(_latest, (table_names, tag, ))
+        resp = {}
+        for tag, result in results.items():
+            resp[tag] = result.get()
+        return resp
+
     def put(self, tag: str, timestamp_ms: int, value: any):
         table_name = self.tr.get_table(timestamp_ms)
         try:
             return put(table_name, tag, timestamp_ms, value)
         except exceptions.TableDoesNotExistException:
             # Table does not exist
-            create_table(table_name, replicated_regions=self.regions)
+            self.__create_table(table_name)
             return put(table_name, tag, timestamp_ms, value)
 
     def put_batch(self, records):
@@ -76,5 +109,5 @@ class TimeSeries:
             try:
                 put_batch(table_name, records)
             except exceptions.TableDoesNotExistException:
-                create_table(table_name, replicated_regions=self.regions)
+                self.__create_table(table_name)
                 put_batch(table_name, records)
--- tests/test_timeseries.py
+++ tests/test_timeseries.py
@@ -6,13 +6,15 @@ from dynamodb_timeseries import TimeSeries, DEFAULT_REGIONS, MONTHLY, HOURLY
 
 
 class TestTimeseries(unittest.TestCase):
-    def test_init(self):
+    @patch('dynamodb_timeseries.dynamodb.CLIENT')
+    def test_init(self, _client):
       ts = TimeSeries('testing')
       self.assertEqual(ts.table_name_prefix, 'testing')
       self.assertEqual(ts.regions, DEFAULT_REGIONS)
       self.assertEqual(ts.tr.interval, MONTHLY)
 
-    def test_non_default_init(self):
+    @patch('dynamodb_timeseries.dynamodb.CLIENT')
+    def test_non_default_init(self, _client):
       ts = TimeSeries('testing', interval=HOURLY, regions=['us-east-1'])
       self.assertEqual(ts.table_name_prefix, 'testing')
       self.assertEqual(ts.regions, ['us-east-1'])
@@ -22,6 +24,7 @@ class TestTimeseries(unittest.TestCase):
     @patch('dynamodb_timeseries.dynamodb.DDB_RESOURCE')
     def test_query(self, resource, client):
       resource.Table.return_value = table = Mock()
+      client.list_tables.return_value = {'TableNames': ['testing-1970-01']}
       table.query = Mock()
       table.query.return_value = {'Items': []}
       ts = TimeSeries('testing')
